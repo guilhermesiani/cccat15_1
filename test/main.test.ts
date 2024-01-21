645d784373b92960c120325bc58c934948dbe92a
Guilherme Siani <contact@guilhermesiani.com>
2024-01-21 13:28:04 -0300

    feat: improve tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -7,12 +7,13 @@ afterEach(async () => {
   await connection.$pool.end();
 })
 
-test("Create an account", async function () {
+test("Create a valid driver account", async function () {
 	const createResponse = await axios.post("http://localhost:3000/accounts", {
     name: "Guilherme Siani",
     email: "user@domain.com",
     cpf: "012.051.530-04",
     isDriver: true,
+    isPassenger: false,
     carPlate: "ASD9456"
 	});
   expect(createResponse.status).toBe(200);
@@ -27,4 +28,74 @@ test("Create an account", async function () {
     is_passenger: false,
     name: "Guilherme Siani"
   });
+});
+
+test("Create a valid passenger account", async function () {
+	const createResponse = await axios.post("http://localhost:3000/accounts", {
+    name: "Guilherme Siani",
+    email: "user@domain.com",
+    cpf: "012.051.530-04",
+    isDriver: false,
+    isPassenger: true,
+	});
+  expect(createResponse.status).toBe(200);
+  expect(createResponse.data.accountId).toBeDefined();
+  const response = await axios.get(`http://localhost:3000/accounts/${createResponse.data.accountId}`);
+  expect(response.data).toEqual({
+    account_id: createResponse.data.accountId,
+    car_plate: null,
+    cpf: "012.051.530-04",
+    email: "user@domain.com",
+    is_driver: false,
+    is_passenger: true,
+    name: "Guilherme Siani"
+  });
+});
+
+test("Create account with invalid car plate should return error", async function () {
+	const createResponse = await axios.post("http://localhost:3000/accounts", {
+    name: "Guilherme Siani",
+    email: "user@domain.com",
+    cpf: "012.051.530-04",
+    isDriver: true,
+    carPlate: "XXXX"
+	});
+  expect(createResponse.status).toBe(200);
+  expect(createResponse.data).toBe(-5);
+});
+
+test("Create account with invalid name should return error", async function () {
+	const createResponse = await axios.post("http://localhost:3000/accounts", {
+    name: "INVALID",
+    email: "user@domain.com",
+    cpf: "012.051.530-04",
+    isDriver: true,
+    carPlate: "ASD9456"
+	});
+  expect(createResponse.status).toBe(200);
+  expect(createResponse.data).toBe(-3);
+});
+
+test("Create account with invalid email should return error", async function () {
+	const createResponse = await axios.post("http://localhost:3000/accounts", {
+    name: "Guilherme Siani",
+    email: "INVALID",
+    cpf: "012.051.530-04",
+    isDriver: true,
+    carPlate: "ASD9456"
+	});
+  expect(createResponse.status).toBe(200);
+  expect(createResponse.data).toBe(-2);
+});
+
+test("Create account with invalid cpf should return error", async function () {
+	const createResponse = await axios.post("http://localhost:3000/accounts", {
+    name: "Guilherme Siani",
+    email: "user@domain.com",
+    cpf: "INVALID",
+    isDriver: true,
+    carPlate: "ASD9456"
+	});
+  expect(createResponse.status).toBe(200);
+  expect(createResponse.data).toBe(-1);
 });
