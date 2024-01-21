b8fb870e182b9d5263f866732ca9afef42a6826b
Guilherme Siani <contact@guilhermesiani.com>
2024-01-21 14:32:59 -0300

    refactor: remove unused imports and local variables

--- src/getAccount.ts
+++ src/getAccount.ts
@@ -1,11 +1,9 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
-import { validateCpf } from "./validateCpf";
 
 export async function getAccount (input: any): Promise<any> {
 	const connection = pgp()("postgres://admin:root@localhost:5432/test_db");
 	try {
-		const id = crypto.randomUUID();
     const result = await connection.query("select * from account where account_id = $1", [input.accountId])
     return result.length > 0 ? result[0] : null;
 	} finally {
